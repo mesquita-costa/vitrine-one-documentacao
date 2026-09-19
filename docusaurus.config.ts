ad21eaa5dbe666780c1c5fedc42940faa0715722
Danielle Mesquita <danielle.cmesquita@gmail.com>
2026-09-19 15:29:46 -0300

    feat:up

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://github.com/mesquita-costa',
+  url: 'https://mesquita-costa.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/vitrine-one-documentacao/',
